a12553b03bf21a8ad6d3e5ac2201b5afb9fc933b
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-07 18:53:39 +0100

    connect to mongodb, show todos

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -1,3 +1,5 @@
+import clientPromise from '$lib/db';
+
 export async function get(request) {
 	// http://localhost:3000/todos?completed=true
 	// http://localhost:3000/todos?completed=false
@@ -12,16 +14,33 @@ export async function get(request) {
 	// const completed = request.query.get('complete') === 'true' ? true : false;
 	// becomes this
 	const completed = request.url.searchParams.get('completed') === 'true' ? true : false;
-	console.log(completed);
+	// console.log(completed);
+
+	// we import the clientPromise from $lib/db.js
+	const dbConnection = await clientPromise;
+
+	const db = dbConnection.db(process.env['MONGODB_DB']);
+	const dbName = db.databaseName;
+	// console.log(dbName);
+
+	const collectionName = process.env['MONGO_DB_TODOS_COLLECTION'];
+	console.log(collectionName);
+
+	const collection = db.collection(collectionName);
+	// console.log(collection);
+
+	// show all documents in the collection in an array
+	const todos = await collection.find().toArray();
+	// console.log(todos);
+
+	// show all completed todos in the collection in an array
+	// const todos = await collection.find(completed).toArray();
+	// console.log(todos);
 
 	return {
 		status: 200,
 		body: {
-			completed: completed
+			todos
 		}
 	};
 }
-
-// export async function post(request) {}
-// export async function put(request) {}
-// export async function del(request) {}
---
+++ src/routes/todos/index.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let todos;
+</script>
+
+{JSON.stringify(todos)}
