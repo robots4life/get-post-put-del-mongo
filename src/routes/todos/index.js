8570bc02054156d6ce8fc65853e28e9cbabad6b5
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-08 10:56:04 +0100

    try catch get request

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -1,46 +1,55 @@
 import clientPromise from '$lib/db';
 
 export async function get(request) {
-	// http://localhost:3000/todos?completed=true
-	// http://localhost:3000/todos?completed=false
+	try {
+		// http://localhost:3000/todos?completed=true
+		// http://localhost:3000/todos?completed=false
 
-	// console.log(request);
+		// console.log(request);
 
-	// const params = request.url.searchParams;
-	// console.log(params);
+		// const params = request.url.searchParams;
+		// console.log(params);
 
-	// event.query has been replaced by event.url.searchParams
-	// so this
-	// const completed = request.query.get('complete') === 'true' ? true : false;
-	// becomes this
-	const completed = request.url.searchParams.get('completed') === 'true' ? true : false;
-	// console.log(completed);
+		// event.query has been replaced by event.url.searchParams
+		// so this
+		// const completed = request.query.get('complete') === 'true' ? true : false;
+		// becomes this
+		const completed = request.url.searchParams.get('completed') === 'true' ? true : false;
+		// console.log(completed);
 
-	// we import the clientPromise from $lib/db.js
-	const dbConnection = await clientPromise;
+		// we import the clientPromise from $lib/db.js
+		const dbConnection = await clientPromise;
 
-	const db = dbConnection.db(process.env['MONGODB_DB']);
-	const dbName = db.databaseName;
-	// console.log(dbName);
+		const db = dbConnection.db(process.env['MONGODB_DB']);
+		const dbName = db.databaseName;
+		// console.log(dbName);
 
-	const collectionName = process.env['MONGO_DB_TODOS_COLLECTION'];
-	console.log(collectionName);
+		const collectionName = process.env['MONGO_DB_TODOS_COLLECTION'];
+		console.log(collectionName);
 
-	const collection = db.collection(collectionName);
-	// console.log(collection);
+		const collection = db.collection(collectionName);
+		// console.log(collection);
 
-	// show all documents in the collection in an array
-	const todos = await collection.find().toArray();
-	// console.log(todos);
+		// show all documents in the collection in an array
+		const todos = await collection.find().toArray();
+		// console.log(todos);
 
-	// show all completed todos in the collection in an array
-	// const todos = await collection.find(completed).toArray();
-	// console.log(todos);
+		// show all completed todos in the collection in an array
+		// const todos = await collection.find(completed).toArray();
+		// console.log(todos);
 
-	return {
-		status: 200,
-		body: {
-			todos
-		}
-	};
+		return {
+			status: 200,
+			body: {
+				todos
+			}
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			body: {
+				error: 'Server error : ' + error
+			}
+		};
+	}
 }
