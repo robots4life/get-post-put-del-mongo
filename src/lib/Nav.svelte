606f5b15c73be5691840e4bce5c5cbcd1b9d7052
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-08 11:57:01 +0100

    try catch post request, Nav

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li><a href="/">HOME</a></li>
+		<li><a href="/todos">TODOS</a></li>
+		<li><a href="/completed">COMPLETED TODOS</a></li>
+	</ul>
+</nav>
--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -53,3 +53,41 @@ export async function get(request) {
 		};
 	}
 }
+
+export async function post(request) {
+	try {
+		// we import the clientPromise from $lib/db.js
+		const dbConnection = await clientPromise;
+
+		const db = dbConnection.db(process.env['MONGODB_DB']);
+		// const dbName = db.databaseName;
+		// console.log(dbName);
+
+		const collectionName = process.env['MONGO_DB_TODOS_COLLECTION'];
+		console.log(collectionName);
+
+		const collection = db.collection(collectionName);
+		// console.log(collection);
+
+		// on the client we JSON.stringify to send the data
+		// on the server we JSON.parse to accept the data
+		const todo = JSON.parse(request.body);
+
+		// insert todo into the collection of the db
+		await collection.insertOne(todo);
+
+		return {
+			status: 200,
+			body: {
+				status: 'Success'
+			}
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			body: {
+				error: 'Server error : ' + error
+			}
+		};
+	}
+}
